0eb3ce62fa2397f743bd518585c3e2732c33c01b
Benjamin Milde <benni@kobrakai.de>
2016-07-26 19:24:08 +0100

    Deployed 18d9482 with MkDocs version: 0.15.3

--- README.md
+++
@@ -1,3 +0,0 @@
-# Migrations
-
-For information on the module please visit: https://processwire.com/talk/topic/13045-migrations/
--- migrations/2015-10-21_12-16.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Migration_2015_10_21_12_16 extends ModuleMigration {
-
-	public static $description = "Install/Uninstall a module with a ModuleMigration";
-
-	protected function getModuleName(){ return 'LazyCron'; }
-
-}
